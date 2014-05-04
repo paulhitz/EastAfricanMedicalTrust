3fe4c70d4c489bb8b3aa68fe3fe763901199edf5
Paul Hitz <ahappycamper@gmail.com>
2014-05-05 00:49:07 +0100

    Allow Users to Add Comments
    Uses a 3rd party commenting system

--- comments/index.php
+++ comments/index.php
@@ -1,24 +1,14 @@
-<?php include("../header.html"); ?>
-
+<?php $title = "Comments"; include("../header.html"); ?>
   <h1>Comments</h1>
 
   <p>Comments from supporters of the charity are listed below. Feel free to add your own comments.</p>
 
-  <div id="comments">
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-      <p>Enter your message details here...</p>
-      <h2 class="warning">Messages disabled due to abuse</h2>
-    </form>
-  </div>
   <div style="padding-left: 10px; padding-right: 10px;">
-  <h2>Previous comments... </h2>
-<?php
-//Include the contents of the comments file and suppress any errors.
-if (!(@ include("comments.txt")))
-{
-    echo "<p style=\"text-align: center;\" class=\"warning\"><strong>*** There was an error displaying the comments ***</strong></p>";
-}
-?>
+    <!-- begin htmlcommentbox.com -->
+     <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+     <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+     <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=(hcb_user.PAGE || ""+window.location), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24arO%2Fo7M0I0k3RcwTcU9Yx0"+"&opts=278&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+    <!-- end htmlcommentbox.com -->
   </div>
 
 <?php include("../footer.html"); ?>
