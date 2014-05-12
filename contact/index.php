f5f71567936e1841719daaf6ab3085acc9cc9a92
Paul Hitz <ahappycamper@gmail.com>
2014-05-12 23:25:15 +0100

    Modify contact details

--- contact/index.php
+++ contact/index.php
@@ -1,14 +1,14 @@
 <?php $title = "Contact Us"; include("../header.html"); ?>
   <h1>Contact Us</h1>
 
-  <p><strong>Kevin O'Doherty:</strong><br/>
-     State Solicitor for Wexford<br/>
-     Melrose<br/>
-     2 Charlotte Row<br/>
-     Gorey<br/>
-     Co Wexford<br/>
-     Ireland<br/>
-   </p>
-   <p>Email: <a href="mailto:william.howlett@east-african-medical-trust.org">william.howlett@east-african-medical-trust.org</a></p>
- 
+  <p><strong>Solicitor:</strong><br/>
+    Kevin O'Doherty<br/>
+    Melrose<br/>
+    2 Charlotte Row<br/>
+    Gorey<br/>
+    Co. Wexford<br/>
+    Ireland<br/>
+  </p>
+  <p><strong>Email:</strong> <a href="mailto:william.howlett@east-african-medical-trust.org">william.howlett@east-african-medical-trust.org</a></p>
+
 <?php include("../footer.html"); ?>
